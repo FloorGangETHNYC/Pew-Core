0254aab0ce5a3eac17405ffe3e60e77e5ad376cd
Kenk <kenghin_lim@hotmail.com>
2022-06-26 03:25:23 +0800

    fix: Fix core contracts

--- contracts/PewCore.sol
+++ contracts/PewCore.sol
@@ -6,9 +6,14 @@ import "./PewNFTFactory.sol";
 contract PewCore {
     PewNFTFactory public PEW_NFT_FACTORY;
 
-    mapping(uint256 => string) daoIds;
+    struct DAO {
+        address contractAddress;
+        string detailsHash;
+    }
 
-    uint256 daoCounter;
+    mapping(uint256 => DAO) public daoIds;
+
+    uint256 public daoCounter;
 
     event DAOCreated(string name, string symbol, address dao);
     event DAOJoined(address indexed member, uint256 indexed _id);
@@ -17,7 +22,7 @@ contract PewCore {
 
     /**
      * @notice Create a new DAO.
-     * @param name  name The name of the DAO.
+     * @param name The name of the DAO.
      * @param symbol The symbol of the DAO.
      * @param metadataHash Metadata Hash of the DAO
      * @dev Include a offchain signer to verify if the structure of the datafile is correct
@@ -33,18 +38,28 @@ contract PewCore {
             address(this)
         );
 
-        daoIds[daoCounter] = metadataHash;
+        daoIds[daoCounter] = DAO(address(pewNFT), metadataHash);
         daoCounter++;
-
         emit DAOCreated(name, symbol, address(pewNFT));
     }
 
     function joinDAO(uint256 _id) public {
+        require(
+            PEW_NFT_FACTORY.getCollection(_id).balanceOf(msg.sender) == 0,
+            "User already minted"
+        );
         PEW_NFT_FACTORY.getCollection(_id).mint(msg.sender);
 
         emit DAOJoined(msg.sender, _id);
     }
 
+    /**
+     * @notice Add a contributement statement
+     * @param _id Id of the DAO
+     * @param _tokenId TokenId of the token
+     * @param ipfsHash Metadata Hash of the contribution
+     * @dev Include a offchain signer to verify if the structure of the datafile is correct
+     */
     function addContribution(
         uint256 _id,
         uint256 _tokenId,
@@ -57,6 +72,12 @@ contract PewCore {
         PEW_NFT_FACTORY.getCollection(_id).addContribution(ipfsHash, _tokenId);
     }
 
+    /**
+     * @notice Upvote a contribution
+     * @param _id Id of the DAO
+     * @param _tokenId TokenId of the token
+     * @param _contributionIndex Index of the contribution
+     */
     function upvote(
         uint256 _id,
         uint256 _tokenId,
@@ -65,6 +86,12 @@ contract PewCore {
         PEW_NFT_FACTORY.getCollection(_id).upvote(_tokenId, _contributionIndex);
     }
 
+    /**
+     * @notice Downvote a contribution
+     * @param _id Id of the DAO
+     * @param _tokenId TokenId of the token
+     * @param _contributionIndex Index of the contribution
+     */
     function downvote(
         uint256 _id,
         uint256 _tokenId,
