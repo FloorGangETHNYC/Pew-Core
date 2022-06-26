d6f6343b5a0c21a684a57bdb339edd909f6e493e
Kenk <kenghin_lim@hotmail.com>
2022-06-26 08:10:30 +0800

    feat: Update worldcoin in deploy script

--- deploy/1_PewCore.ts
+++ deploy/1_PewCore.ts
@@ -10,10 +10,15 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }: any) => {
   const chainId = await getChainId();
 
   console.log("Chain ID:", chainId);
+  let worldId;
+
+  if (chainId === "80001" || chainId === "69" || chainId === "31337") {
+    worldId = "0xABB70f7F39035586Da57B3c8136035f87AC0d2Aa";
+  }
 
   let pewCore = await deploy("PewCore", {
     from: deployer,
-    args: [],
+    args: [worldId],
   });
 
   gasLogger.addDeployment(pewCore);
